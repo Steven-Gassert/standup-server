051117d2563b9feb5e4fef9e7b38218f84c0ee4a
stevengassert94@gmail.com <Steven.Gassert@ibm.com>
2018-11-14 08:18:16 -0500

    /slack route validates user_id

--- src/routes/slack.js
+++ src/routes/slack.js
@@ -2,6 +2,10 @@ const authLinks = require('../authLinks');
 
 const express = require('express');
 let router = express.Router();
+const bodyParser = require('body-parser');
+const urlencoded = bodyParser.urlencoded({
+  extended: true
+});
 
 // set winston logging level
 const winston = require('winston');
@@ -11,38 +15,42 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()]
 });
 
-// connect to mongodb
-const mongoose = require('mongoose');
-const standupConfig = require('../../models/standupConfig');
-try {
-  const connectionString = require('../dbconfig');
-  mongoose.connect(connectionString); // connect to mongo db using credentials returned from ../dbconfig.js
-} catch(error) {
-  logger.log('error', 'mongoose was unable to connect our mongodb', {error:error});
-}
+const standupConfig = require('../../models/standupConfig'); // import mongoose model
 
 // Route which slack app 'slash command' hits to GET STANDUP
-router.post('/', function(req,res) {
+router.post('/', urlencoded, function (req, res) {
   let user_id = req.body.user_id;
-  logger.log('debug',{user_id:user_id});
+  logger.log('debug', 'request recieved', {
+    id: user_id
+  });
 
   // make sure there is a valid user_id in this request
   if (user_id && user_id !== '') {
-    let user_config = standupConfig.findOne({user_id: user_id}); // retrieve user_config from MongoDB
 
-    // there was a user_config in our db for this user_id
-    if (user_config) {
-      logger.log('debug', 'retrieving standup for user after getting user_config', {user_config:user_config});
-      res.send(200).json({user_config: user_config});
+    // retrieve user_config from MongoDB
+    standupConfig.findOne({ user_id: user_id }, function mongoCallback(err, user_config) {
+      if (err) {
+        logger.log('error', 'There was an error attempting to retrieve user_config', { user_id: user_id, error: err });
+      } else {
+        logger.log('debug', 'got user_config', { user_config: user_config });
 
-    // there was not a user_config in our db for this user_id
-    } else {
-      authLinks.send();
-      res.sendStatus(200);
-    }
+        // there was a user_config in our db for this user_id
+        if (user_config) {
+          logger.log('debug', 'retrieving standup for user after getting user_config', {user_config:user_config});
 
+          // there was not a user_config in our db for this user_id
+        } else {
+          authLinks.send();
+          logger.log('debug', 'authLinks.send() was called');
+        }
+      }
+    });
+    logger.log('debug', 'this logging message, should it come first?'); 
+    res.sendStatus(200); // will get back to slack immediatly, all other interaction in mongoCallback will be done w/ response url
+
+  // There was no user_id in this request
   } else {
-    res.sendStatus(422);
+    res.sendStatus(400);
   }
 
 });
--- test/unit/slack.test.js
+++ test/unit/slack.test.js
@@ -1,59 +1,40 @@
-const proxyquire = require('proxyquire');
-const sinon = require('sinon');
-const request = require('supertest');
 const express = require('express');
-const bodyparser = require('body-parser');
-const assert = require('assert');
+const request = require('supertest');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+// const bodyparser = require('body-parser');
 
-describe('/slack endpoint', function() {
-  let app, slack;
-  const standupConfig = require('../../models/standupConfig');
-  const mongoose = require('mongoose');
+let modelStub;
 
-  // why did I get reference errors when this was inside beforeEach() ?
-  let connectStub = sinon.stub(mongoose, 'connect').returns('stubbed connect method');
-  let findOneStub = sinon.stub(standupConfig,'findOne').returns(null); // will return null instead of [] because we are using standupConfig.findOne()
-  let sendSpy = sinon.spy();
-  beforeEach(function() {
-    slack = proxyquire('../../src/routes/slack',{
-      'mongoose': {
-        'connect': connectStub
-      },
-      'standupConfig': {
-        'findOne': findOneStub
-      },
-      'authLinks': {
-        'send': sendSpy
-      }
-    });
-    app = express();
-    app.use(bodyparser.json());
-    app.use('/slack',slack);
-  });  
-
-  it('returns OK 200 message when posting to /slack with a non-empty string as a user_id', function(done) {
-    request(app)
-      .post('/slack')
-      .send({'user_id': 'testid'})
-      .expect(200, done);
-  });
+function setup(slack) {
+  let app = express();
+  app.use('/slack',slack);
+  return app;
   
-  it('returns a 422 Bad input response when posting to /slack with no user_id', function(done) {
-    request(app)
-      .post('/slack')
-      .expect(422,done);
+}
+
+beforeEach(() => {
+  modelStub = sinon.stub;
+});
+
+test('should return OK 200 when making a request with user_id', function(done) {
+  let slack = proxyquire('../../src/routes/slack', {
+    '/../../models/standupConfig': modelStub
   });
+  let app = setup(slack);
+  request(app)
+    .post('/slack')
+    .type('application/x-www-form-urlencoded')
+    .send({user_id:'fake_id'})
+    .expect(200,done);
+});
 
-  it('if a user_id with no configuration info saved calls standup-helper, authLinks.send() is called', function(done) {
-    request(app)
-      .post('/slack')
-      .send({'user_id': 'testid'})
-      .expect(200);
-
-    assert(sendSpy.called, done);
-    done();
-    
-    
+test('should return 400 bad request when a request with no user_id is made', function(done) {
+  let slack = proxyquire('../../src/routes/slack', {
+    '/../../models/standupConfig': modelStub
   });
-
+  let app = setup(slack);
+  request(app)
+    .post('/slack')
+    .expect(400,done);
 });
